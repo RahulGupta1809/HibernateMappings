066e5c7b5eb4948d121148b662bfc53ac7086e52
Rahul Gupta <rahuldevit2000@gmail.com>
2026-03-17 21:41:46 +0530

    Update StudentAddressBean.java
    Add Comments

--- src/main/java/com/cglia/hibernate/bean/StudentAddressBean.java
+++ src/main/java/com/cglia/hibernate/bean/StudentAddressBean.java
@@ -7,9 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
+//Bean Class
 @Entity
 @Table(name = "student_address", catalog = "rahul_g")
 public class StudentAddressBean {
